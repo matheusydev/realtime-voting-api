960416edb1dcd2fd649dcb3288c0ae698247a39b
Matheusydev <matheusylanaraujo@gmail.com>
2026-07-05 12:49:25 -0300

    chore: configuração inicial do projeto com Express e dotenv

---
+++ src/server.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('API rodando');
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
